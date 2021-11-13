c3aaedea43e3428534d6675470313f2b4102dbb5
Kris Pietrzak <krispdev@hotmail.com>
2021-11-13 18:37:45 +1000

    added shadow to nav on scroll

--- Landing pages/Food Delivery landing page/main.js
+++ Landing pages/Food Delivery landing page/main.js
@@ -48,7 +48,18 @@ const scrollActive = function () {
 
 window.addEventListener("scroll", scrollActive);
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*=============== ADD SHADOW TO HEADER NAV ===============*/
+const scrollHeader = function () {
+   const header = document.getElementById("header");
+   // If scroll >= 80vh, add the scroll-header class
+   if (this.scrollY >= 80) {
+      header.classList.add("scroll-header");
+   } else {
+      header.classList.remove("scroll-header");
+   }
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 
